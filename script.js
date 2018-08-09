3be9a0fed633138bf68c3972816798c137af667c
AGCH <anastasiyache@gmail.com>
2018-08-09 15:19:12 +0300

    With handlebars

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var RecipeApp = function () {
-
     var recipes = [
         { 
             id: 1,
@@ -12,7 +11,6 @@ var RecipeApp = function () {
             ] 
         }
     ];
-
     var $recipes = $('.recipes');
 
     //id's for recipes
@@ -21,10 +19,10 @@ var RecipeApp = function () {
     //id's for ingredients
     var ingId = 0;
 
-    var createRecipe = function(name, image){
+    var createRecipe = function(name, link){
         var recipe = {
             name: name,
-            image: image, 
+            image: link, 
             ingredients: [],
             id: recId
         };
@@ -34,61 +32,74 @@ var RecipeApp = function () {
         ingId = 0; //updating IngId with New Recipe
         recipes.push(recipe);
     };
-
-    var createIngredients = function(){
-        var recipe = {
+    
+    var createIngredients = function(index, text){
+        var ingredient = {
             id: ingId,
-            name: name
+            name: text
         };
 
         //keeps recipe ids unique 
         ingId ++; 
-
-        recipes.ingredients.push(ingredient);
+        app.recipes[index].ingredients.push(ingredient);
     };
-
-
-
     var renderRecipes = function () {
         //empty recipes div
         $recipes.empty();
         var source = $('#recipes-template').html();
         var template = Handlebars.compile(source);
         var newHTML = template(this);
-        this.$('.recipes row').append(newHTML); 
-        bindEvents();
-        }
-
+        $('.list').append(newHTML); 
+        bindEvents(); bindEventsDelete()
+        };
     return {
         recipes: recipes,
         createRecipe: createRecipe,
         renderRecipes: renderRecipes,
-        createIngredients: createIngredients,
+        createIngredients: createIngredients
     }
 };
 
 var app = RecipeApp();
 //--------EVENTS
-//add a recipe
-// function bindEvents() {
-//     $('.close').off();
-//     debugger;
-//     $('.close').click("click",function () {
-//      
-//     });
-// }
+
+function bindEvents() {
+    $('.add-ingredients').off();
+    $('.add-ingredients').on("click",function () {
+        var text = $(this).closest(".mb-3").find('input').val();
+        let recipeId = $(this).closest('div.recipe').data().id;
+        let obj = findRecipeById(recipeId);
+        app.createIngredients(obj.index, text);
+        app.renderRecipes();
+    });
+}
+function removeIngredient(btn) {
+    let recipeId = $(btn).closest('div.recipe').data().id;
+    let commentIndex = $(btn).closest("li").index();
+    let obj = findRecipeById(recipeId);
+    app.recipes[obj.index].ingredients.splice(commentIndex, 1);
+    app.renderRecipes();
+}
+var findRecipeById = function (id) {
+    for (let i = 0; i < app.recipes.length; i += 1) {
+        if (app.recipes[i].id === id) {
+            return ({index: i, recipe: app.recipes[i]});
+        }
+    }
+};
+
 
 $('.add-recipe').on('click',function () {
     var name = $('#recipe-name').val();
-    var image = $('#recipe-image').val();
+    var link='https://static01.nyt.com/images/2016/11/29/dining/recipelab-chick-noodle-still/recipelab-chick-noodle-still-master675.jpg';
+    var image = link;//$('#recipe-image').val();
     //add recipe to array and render
-    app.createRecipe(name, image);
+    app.createRecipe(name, link);
     app.renderRecipes();
 });
-
-$('body').on('click', '.close', function () {
-    console.log("delete");
-    app.removeIngredient(this);
+function bindEventsDelete() {
+    $('.close').off();
+    $('.recipe').on('click', '.close', function () {
+    removeIngredient(this);
 });
-
-
+ }
